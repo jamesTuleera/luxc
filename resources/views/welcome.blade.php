5b3f97bce80ea33893a7298b451114407dfc4895
barituleera <barituleera@gmail.com>
2023-12-21 08:17:53 +0100

    all

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,45 +47,23 @@
           <!-- Uncomment below if you prefer to use an image logo -->
      <a href="{{ route('home') }}" class="logo"><img src="{{ asset('home_assets/assets/img/logo.jpeg') }}" alt="" class="img-fluid"></a>
 
-          <nav id="navbar" class="navbar">
-            <ul>
-              <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-              <li><a class="nav-link scrollto" href="#about">About</a></li>
-              <li class="dropdown"><a href="#"><span>Membership</span> <i class="bi bi-chevron-down"></i></a>
+     <nav id="navbar" class="navbar">
+        <ul>
+            <li><a class="nav-link scrollto active" href="{{ route('home') }}">Home</a></li>
+            <li><a class="nav-link scrollto" href="{{ route('about') }}">About</a></li>
+            <li class="dropdown"><a href="#"><span>Membership</span> <i
+                        class="bi bi-chevron-down"></i></a>
                 <ul>
-              <li><a href="#">For Individual</a></li>
-              <li><a href="#">For Business</a></li>
+                    <li><a href="{{ route('membership') }}">For Individual</a></li>
+                    <li><a href="{{ route('membership') }}">For Business</a></li>
                 </ul>
-              </li>
-              <li><a class="nav-link scrollto" href="#services">Services</a></li>
-              {{-- <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
-              <li><a class="nav-link scrollto" href="#team">Team</a></li>
-              <li><a class="nav-link  " href="blog.html">Blog</a></li> --}}
-
-
-
-              {{-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-                <ul>
-                  <li><a href="#">Drop Down 1</a></li>
-                  <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                    <ul>
-                      <li><a href="#">Deep Drop Down 1</a></li>
-                      <li><a href="#">Deep Drop Down 2</a></li>
-                      <li><a href="#">Deep Drop Down 3</a></li>
-                      <li><a href="#">Deep Drop Down 4</a></li>
-                      <li><a href="#">Deep Drop Down 5</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="#">Drop Down 2</a></li>
-                  <li><a href="#">Drop Down 3</a></li>
-                  <li><a href="#">Drop Down 4</a></li>
-                </ul>
-              </li> --}}
-              <li><a class="nav-link scrollto" href="#contact">Register</a></li>
-              <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-            </ul>
-            <i class="bi bi-list mobile-nav-toggle"></i>
-          </nav><!-- .navbar -->
+            </li>
+            <li><a class="nav-link scrollto" href="{{ route('services') }}">Services</a></li>
+            <li><a class="nav-link scrollto" href="{{ route('membership') }}">Register</a></li>
+            <li><a class="nav-link scrollto" href="{{ route('contact') }}">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+    </nav><!-- .navbar -->
         </div>
       </div>
 
@@ -930,92 +908,95 @@
 
   </main><!-- End #main -->
 
-  <!-- ======= Footer ======= -->
   <footer id="footer">
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
 
-          <div class="col-lg-3 col-md-6 footer-info">
-            <h3>BizPage</h3>
-            <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
-          </div>
+                    <div class="col-lg-4 col-md-6 footer-info">
+                        <h3>Luxcierge - Luxury</h3>
 
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bi bi-chevron-right"></i> <a href="#">Privacy policy</a></li>
-            </ul>
-          </div>
+                    </div>
 
-          <div class="col-lg-3 col-md-6 footer-contact">
-            <h4>Contact Us</h4>
-            <p>
-              A108 Adam Street <br>
-              New York, NY 535022<br>
-              United States <br>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
-            </p>
+                    <div class="col-lg-4 col-md-6 footer-links">
+                        <h4>Useful Links</h4>
+                        <ul>
+                            <li><i class="bi bi-chevron-right"></i> <a href="{{ route('home') }}">Home</a></li>
+                            <li><i class="bi bi-chevron-right"></i> <a href="{{ route('about') }}">About us</a></li>
+                            <li><i class="bi bi-chevron-right"></i> <a href="{{ route('services') }}">Services</a></li>
+                            <li><i class="bi bi-chevron-right"></i> <a href="{{ route('contact') }}">Contact us</a>
+                            </li>
+                            <li><i class="bi bi-chevron-right"></i> <a href="{{ route('membership') }}">Membership</a>
+                            </li>
+                        </ul>
+                    </div>
 
-            <div class="social-links">
-              <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-              <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-              <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-              <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-              <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                    <div class="col-lg-4 col-md-6 footer-contact">
+                        <h4>Contact Us</h4>
+                        <p>
+                            <strong>Address:</strong>
+                            123 Carolina St, <br> San Francisco, <br> CA 94103, <br> United States.<br>
+                            <strong>Phone:</strong> +234 913 177 0885<br>
+                            <strong>Email:</strong> hello@luxcierge.ng<br>
+                        </p>
+
+                        <div class="social-links">
+                            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                        </div>
+
+                    </div>
+
+                    {{-- <div class="col-lg-4 col-md-6 footer-newsletter">
+                        <h4>Our Newsletter</h4>
+                        <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna veniam enim veniam
+                            illum dolore legam minim quorum culpa amet magna export quem marada parida nodela caramase
+                            seza.</p>
+                        <form action="" method="post">
+                            <input type="email" name="email"><input type="submit" value="Subscribe">
+                        </form>
+                    </div> --}}
+
+                </div>
             </div>
-
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-newsletter">
-            <h4>Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna veniam enim veniam illum dolore legam minim quorum culpa amet magna export quem marada parida nodela caramase seza.</p>
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
-          </div>
-
         </div>
-      </div>
-    </div>
 
-    <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong>BizPage</strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-        <!--
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong>BizPage</strong>. All Rights Reserved
+            </div>
+            <div class="credits">
+                <!--
         All the links in the footer should remain intact.
         You can delete the links only if you purchased the pro version.
         Licensing information: https://bootstrapmade.com/license/
         Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=BizPage
       -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
-    </div>
-  </footer><!-- End Footer -->
+                {{-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> --}}
+            </div>
+        </div>
+    </footer><!-- End Footer -->
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-  <!-- Uncomment below i you want to use a preloader -->
-  <!-- <div id="preloader"></div> -->
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+    <!-- Uncomment below i you want to use a preloader -->
+    <!-- <div id="preloader"></div> -->
 
-  <!-- Vendor JS Files -->
-  <script src="{{ asset('home_assets/assets/vendor/purecounter/purecounter.js') }}"></script>
-  <script src="{{ asset('home_assets/assets/vendor/aos/aos.js') }}"></script>
-  <script src="{{ asset('home_assets/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('home_assets/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
-  <script src="{{ asset('home_assets/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
-  <script src="{{ asset('home_assets/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
-  <script src="{{ asset('home_assets/assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
-  <script src="{{ asset('home_assets/assets/vendor/php-email-form/validate.js') }}"></script>
+    <!-- Vendor JS Files -->
+    <script src="{{ asset('home_assets/assets/vendor/purecounter/purecounter.js') }}"></script>
+    <script src="{{ asset('home_assets/assets/vendor/aos/aos.js') }}"></script>
+    <script src="{{ asset('home_assets/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('home_assets/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+    <script src="{{ asset('home_assets/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('home_assets/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('home_assets/assets/vendor/waypoints/noframework.waypoints.js') }}"></script>
+    <script src="{{ asset('home_assets/assets/vendor/php-email-form/validate.js') }}"></script>
 
-  <!-- Template Main JS File -->
-  <script src="{{ asset('home_assets/assets/js/main.js') }}"></script>
+    <!-- Template Main JS File -->
+    <script src="{{ asset('home_assets/assets/js/main.js') }}"></script>
 
 </body>
 
